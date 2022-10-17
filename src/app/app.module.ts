113abad8fb479a99218aa82e16beae99c16e134a
adri332211 <amartins03@educarex.es>
2022-10-17 13:39:58 +0200

    eeeeee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+
+import { contadorModule } from './contador/contador/contador.module';
+import { heroesModule } from './heroes/heroes.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    contadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    heroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent implements OnInit {
+export class ListadoComponent  {
 
-  constructor() { }
+heroes:string[]=['Deku','Bakugo','Todoroki','All Might'];
 
-  ngOnInit(): void {
-  }
+heroeBorrado:string='';
+
+borrarHeroe(){
+  this.heroeBorrado=this.heroes.shift() || ' ';
+}
 
 }
