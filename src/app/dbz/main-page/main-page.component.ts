1f13ed1560b9e43dc71567cdbe2b808515602b9b
adri332211 <amartins03@educarex.es>
2022-10-17 16:30:18 +0200

    dbz

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  styleUrls: ['./main-page.component.css'],
+  template:`<h1>Dragon Ball Z</h1>
+  <hr>`
+})
+export class MainPageComponent  {
+
+  
+
+}
