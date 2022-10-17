7414c92823d4b22789069a3a099e88c255f15b8f
adri332211 <amartins03@educarex.es>
2022-10-17 10:07:31 +0200

    heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { contadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    contadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
